4f89974806b2ef2a2879c114c401926f68e27b1f
FengYouJun <845225187@qq.com>
2021-11-29 21:37:47 +0800

    initial commit

---
+++ src/test/java/com/fengyoujun/algorithm/TestDemo.java
@@ -0,0 +1,23 @@
+package com.fengyoujun.algorithm;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author FengYouJun
+ * @since 2021/11/29
+ */
+public class TestDemo {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all...");
+    }
+
+    @Test
+    @DisplayName("test demo1...")
+    public void testDemo1() {
+        System.out.println("test demo1...");
+    }
+}
